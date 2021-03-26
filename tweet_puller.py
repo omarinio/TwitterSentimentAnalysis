0e8ad95f30b867bfb44084a503dee8e52db8a432
Omar Suleiman <jb18940@bristol.ac.uk>
2021-03-26 17:51:44 +0100

    added clean_tweet function

--- tweet_puller.py
+++ tweet_puller.py
@@ -6,6 +6,16 @@ import re
 import string
 import preprocessor as p
 from keys import *
+
+def clean_tweet(tweet):
+    tweet = re.sub(b"https?://", b"", tweet)   #links
+    tweet = re.sub(b"#\S+", b"", tweet)           #hashtags
+    tweet = re.sub(b".?@", b"", tweet)           #at mentions
+    tweet = re.sub(b"RT.+", b"", tweet)           #Retweets
+    tweet = re.sub(b"Video:", b"", tweet)        #Videos
+    tweet = re.sub(b"\n", b"", tweet)             #new lines
+    tweet = re.sub(b"&", b"and", tweet)       #encoded ampersands
+    return tweet
  
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
@@ -21,4 +31,7 @@ new_search = search_words + " -filter:retweets"
 for tweet in tweepy.Cursor(api.search,q=new_search,count=100,
                            lang="en",
                            since_id=0).items(100):
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),tweet.user.screen_name.encode('utf-8'), tweet.user.location.encode('utf-8')])
+    new_tweet = clean_tweet(tweet.text.encode('utf-8'))
+    #csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),tweet.user.screen_name.encode('utf-8'), tweet.user.location.encode('utf-8')])
+    csvWriter.writerow([new_tweet])
+
