bff87752d5c3af15c96b4c278e15f7d345c2016a
Omar Suleiman <jb18940@bristol.ac.uk>
2021-04-05 15:45:07 +0200

    Refactored code still

--- tweet_puller.py
+++ tweet_puller.py
@@ -20,23 +20,26 @@ from keys import *
 def clean_tweet(tweet):
     return " ".join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", tweet).split())
  
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
- 
-api = tweepy.API(auth,wait_on_rate_limit=True)
- 
-csvFile = open('falo.csv', 'a')
-csvWriter = csv.writer(csvFile)
- 
-search_words = "US Election"      # enter your words
-new_search = search_words + " -filter:retweets"
- 
-for tweet in tweepy.Cursor(api.search,q=new_search,
-                           lang="en",
-                           since_id=0,
-                           tweet_mode="extended").items(1):
-    new_tweet = clean_tweet(tweet.full_text)
-    #csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),tweet.user.screen_name.encode('utf-8'), tweet.user.location.encode('utf-8')])
-    csvWriter.writerow([new_tweet])
+def get_tweets():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    
+    api = tweepy.API(auth,wait_on_rate_limit=True)
+    
+    csvFile = open('falo.csv', 'a')
+    csvWriter = csv.writer(csvFile)
+    
+    search_words = "US Election -filter:retweets"      # enter your words
+    
+    for tweet in tweepy.Cursor(api.search,q=search_words,
+                            lang="en",
+                            since_id=0,
+                            tweet_mode="extended").items(1):
+        new_tweet = clean_tweet(tweet.full_text)
+        #csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),tweet.user.screen_name.encode('utf-8'), tweet.user.location.encode('utf-8')])
+        csvWriter.writerow([new_tweet])
+
+if __name__ == '__main__':
+	get_tweets()
     
 
