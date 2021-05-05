ca5393ee6f76ce69d2bdfe6418dc8ebbe488f6a7
mastamysta <benjamin-read@hotmail.co.uk>
2021-05-05 12:18:08 +0100

    modified sentiment.py to work for political lexicon

--- sentiment.py
+++ sentiment.py
@@ -1,14 +1,19 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
 import json
-
-analyzer = SentimentIntensityAnalyzer()
+import polSent as ps
 
 with open("tweets.json") as json_file:
     data = json.load(json_file)
-    for user in data:
-        print(data[user][0]['text'])    
-        vs = analyzer.polarity_scores(data[user][0]['text']) 
-        print(vs)
-        testimonial = TextBlob(data[user][0]['text'])
-        print(testimonial.sentiment)
+    listoftexts = [data[user][0]['text'] for user in data]
+    vss = ps.analyse(listoftexts)
+    i=0
+
+    for i in range(0, vss.shape[0]):
+        print(vss['tweet'][i])
+        print(vss['score'][i])
+        print("\n")
+        print("\n")
+    
+
+    
